376b126a41267205d887d6eb8f1e57a2c18819b5
Vikrant Guleria <vguleria1108@gmail.com>
2022-09-04 09:35:17 +0530

    Ability to add messages with waves added

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -11,13 +11,30 @@ contract WavePortal {
         console.log("Yo yo, First contract in making!");
     }
 
-    function wave() public {
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waver; // The address of the user who waved.
+        string message; // The message the user sent.
+        uint256 timestamp; // The timestamp when the user waved.
+    }
+
+    // Store Waves in an array.
+    Wave[] waves;
+
+    function wave(string memory _message) public {
+        waves.push(Wave(msg.sender, _message, block.timestamp));
         totalWaves += 1;
+        emit NewWave(msg.sender, block.timestamp, _message);
         console.log("%s has waved!", msg.sender);
     }
 
     function getTotalWaves() public view returns (uint256) {
         console.log("We have %d total waves!", totalWaves);
         return totalWaves;
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -10,13 +10,15 @@ const main = async () => {
   let waveCount;
   waveCount = await waveContract.getTotalWaves();
   //   owner waves
-  let waveTxn = await waveContract.wave();
+  let waveTxn = await waveContract.wave("Hello ji, kase ho?");
   await waveTxn.wait();
   waveCount = await waveContract.getTotalWaves();
   //   random person waves
-  waveTxn = await waveContract.connect(randomPerson).wave();
+  waveTxn = await waveContract.connect(randomPerson).wave("Bdhiya");
   await waveTxn.wait();
   waveCount = await waveContract.getTotalWaves();
+  const allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
 };
 
 const runMain = async () => {
