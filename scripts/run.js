67c02a5f7d85e7d452af169277c09a98a0bc9f88
Vikrant Guleria <vguleria1108@gmail.com>
2022-09-04 07:23:19 +0530

    Wave functionality added

--- scripts/run.js
+++ scripts/run.js
@@ -1,10 +1,22 @@
 // npx hardhat run scripts/run.js
 const main = async () => {
   //hre is a global variable that is injected by Hardhat Runtime Environment
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
   console.log("Contract deployed to:", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+  //   owner waves
+  let waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+  waveCount = await waveContract.getTotalWaves();
+  //   random person waves
+  waveTxn = await waveContract.connect(randomPerson).wave();
+  await waveTxn.wait();
+  waveCount = await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
